e0e96ff820ca8e3d091b8acdd999dd73e29a9d76
Loreto <loreto.garde@gmail.com>
2025-02-06 10:29:33 +0100

    Refactor Bonus class: update dimensions and inline style assignments

--- js/Bonus.js
+++ js/Bonus.js
@@ -5,34 +5,38 @@ class Bonus {
     this.w = 30;
     this.h = 30;
     this.speed = 2;
-    this.isCollided = false;  // Bandera para saber si ha colisionado
+    this.isCollided = false;
 
     this.bonus = document.createElement("img");
     this.bonus.style.width = `${this.w}px`;
     this.bonus.style.height = `${this.h}px`;
-    
-    if(type === "points") {
+
+    if (type === "points") {
       this.bonus.src = `./images/point.png`;
-      
-    } else if(type === "extra") {
+    } else if (type === "extra") {
       this.bonus.src = `./images/extra.png`;
       this.bonus.style.width = `50px`;
       this.bonus.style.height = `50px`;
-    } else if(type === "life") {
+      this.w = 50; 
+      this.h = 50;
+    } else if (type === "life") {
       this.bonus.src = `./images/oro.webp`;
       this.bonus.style.width = `50px`;
       this.bonus.style.height = `50px`;
-    } else if(type === "missile") {
+      this.w = 50;
+      this.h = 50;
+    } else if (type === "missile") {
       this.bonus.src = `./images/misil-box.png`;
-    } else if(type === "winner") {
+    } else if (type === "winner") {
       this.bonus.src = `./images/cristal.png`;
       this.bonus.style.width = `20px`;
       this.bonus.style.height = `70px`;
+      this.w = 20;
+      this.h = 70;
     }
 
     gameBoxNode.append(this.bonus);
 
-    // Dimensiones estáticas y posición aleatoria:
     this.bonus.style.position = "absolute";
     this.bonus.style.top = `${this.y}px`;
     this.bonus.style.left = `${this.x}px`;
@@ -44,11 +48,8 @@ class Bonus {
   }
 
   winnerMovement() {
-    if (this.x > 80) { // Si aún no ha alcanzado la posición del jugador, sigue moviéndose
+    if (this.x > 80) {    // Moverlo hacia la izquierda hasta X=80 (cerca del player)
       this.x -= this.speed;
-      this.bonus.style.left = `${this.x}px`;
-    } else {
-      this.x = 80;
       this.bonus.style.left = `${this.x}px`;
     }
   }
